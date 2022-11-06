d839b9fdf99a6cac74df449eb15004588b2ba697
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-11-06 20:28:30 +0000

    Update Notepad.py

--- Notepad/Notepad.py
+++ Notepad/Notepad.py
@@ -1,11 +1,19 @@
 def read_notes(x):
     """Function to retrieve notes if they exist"""
     try:
-        x = open('Notes.txt', 'rb')
+        with open('Notes.txt', 'r') as notes_file:
+            for line in notes_file:
+                x.append(line)
+        return x
+
     except FileNotFoundError:
         print()
-        print("As this is your first time loading this application a new 'Note.txt' file will be generated")
+        print("As this is your first time loading this application, a new 'Note.txt' file will be generated")
         print()
+        with open('Notes.txt', 'w') as notes_file:
+
+
+
 
 
 
@@ -17,6 +25,7 @@ print("*" * 10)
 print("Please enter 'v' to view all notes")
 print("Please enter 'E' to edit a note")
 print("Please enter 'D' to delete a specified note")
+print("Please enter '' to delete a specified note")
 
 notes = []
 read_notes(notes)
