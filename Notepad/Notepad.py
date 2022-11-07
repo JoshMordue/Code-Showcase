b6a164fcc4d496442147b35c7821a9333cb3210b
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-11-07 01:45:18 +0000

    work on project

--- Notepad/Notepad.py
+++ Notepad/Notepad.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def read_notes(x):
     """Function to retrieve notes if they exist, if it does not create an empty """
     try:
@@ -10,21 +13,34 @@ def read_notes(x):
         print()
         print("As this is your first time loading this application, a new 'Note.txt' file will be generated")
         print()
-        open('Notes.txt', 'w').close()
+        agreement = input("For this to work we would need your consent, type 'Y' to Agree otherwise type 'N': ").lower()
+        if agreement == 'y':
+            open('Notes.txt', 'w').close()
+            print()
+            print("File has been successfully generated")
+            print()
+            read_notes(notes)
+        else:
+            print()
+            print('This is a required process for the application to run, please reload the application to try again')
+            sys.exit()
 
 
-print("NotePad!")
-print("*" * 10)
-print("Please enter 'v' to view all notes")
-print("Please enter 'E' to edit a note")
-print("Please enter 'D' to delete a specified note")
-print("Please enter '' to delete a specified note")
+def menu():
+    print("NotePad!")
+    print("*" * 10)
+    print("Please enter 'v' to view all notes")
+    print("Please enter 'E' to edit a note")
+    print("Please enter 'D' to delete a specified note")
+    print("Please enter '' to delete a specified note")
+
+
 
 notes = []
 read_notes(notes)
-
+menu()
 
 while True:
     print(notes)
 
-    choice = input()
+    choice = input("Please enter your choice: ")
