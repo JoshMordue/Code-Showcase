ac8653a0f025d0a8ef4feaad866382722f970a23
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-11-07 13:16:40 +0000

    work on project

--- Notepad/Notepad.py
+++ Notepad/Notepad.py
@@ -28,13 +28,34 @@ def read_notes(x):
 
 def view_notes(x):
     for num, note in enumerate(x):
-        print(x)
         print("{0}: {1}".format(num + 1, note))
+
+
+def edit_notes(x):
+    view_notes(x)
+    edit_choice = int(input("Please specify the number: "))
+    if edit_choice <= len(x):
+        print("You're currently editing entry {0}".format(edit_choice))
+        new_value = input("Please enter the edited note: ")
+        x[edit_choice] = new_value
+        save_changes(x)
+
+    return x
+
+
+def save_changes(x):
+    try:
+        with open('Notes.txt', 'w', encoding='utf-8') as notes_file:
+            notes_file.write('\n')
+        print("The changes have been saved.")
+    except OSError:
+        print("We encountered a problem saving to the directory, please assess your folder write permissions.")
 
 
 def menu():
     print("NotePad!")
     print("*" * 10)
+    print("Please enter 'A' to add a note")
     print("Please enter 'v' to view all notes")
     print("Please enter 'E' to edit a note")
     print("Please enter 'D' to delete a specified note")
@@ -51,3 +72,6 @@ while True:
 
     if choice == "V":
         view_notes(notes)
+
+    if choice == "E":
+        edit_notes(notes)
