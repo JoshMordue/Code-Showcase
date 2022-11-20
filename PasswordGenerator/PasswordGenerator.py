8fa55fe7186b85df87d69702306314b97ae434d5
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-11-20 22:16:24 +0000

    work on project

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -7,35 +7,44 @@ window.title('Password Generator')
 window.geometry('500x500')
 
 
-# def loadVariables():
-#     lower = string.ascii_lowercase
-#     upper = string.ascii_uppercase
-#     numbers = string.digits
-#     symbols = string.punctuation
-#     all_characters = ''
+def generator():
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    numbers = string.digits
+    symbols = string.punctuation
+    all_characters = ''
 
+    option1 = op1.getvar()
+    option2 = op2.getvar()
+    option3 = op3.getvar()
+    option4 = op4.getvar()
+    password_length = Pass_length.get()
 
-def user_choice():
-    if op1 == 1:
+    print(option1)
+    print(op1)
+    print(lower)
+    if option1.get():
+        print(lower)
         all_characters += lower
-    if op2 == 1:
+    if option2 == 1:
         all_characters += upper
-    if op3 == 1:
+    if option3 == 1:
         all_characters += numbers
-    if op4 == 1:
+    if option4 == 1:
         all_characters += symbols
 
-    gen_password = random.sample(all_characters, passwordlength)
-    return gen_password
+    GenPass.insert(0, all_characters)
+    print(all_characters)
 
-def loadassets():
+
+def build():
     description.pack()
     op1.pack()
     op2.pack()
     op3.pack()
-    Descript.pack()
     op4.pack()
-    Password_length.pack()
+    Descript.pack()
+    Pass_length.pack()
     PassDescript.pack()
     GenPass.pack()
     window.mainloop()
@@ -44,23 +53,15 @@ def loadassets():
 description = tk.Label(window, bg='white', width=50, height='5', text="This application is for generating passwords.\n"
                                                             "Please select from the checkboxes below your preferences")
 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-numbers = string.digits
-symbols = string.punctuation
-all_characters = ''
-gen_password = ''
-
 
 op1 = tk.Checkbutton(window, text='Lower-Case Characters', onvalue=1, offvalue=0)
 op2 = tk.Checkbutton(window, text='Upper-Case Characters', onvalue=1, offvalue=0)
 op3 = tk.Checkbutton(window, text='Numbers', onvalue=1, offvalue=0)
 op4 = tk.Checkbutton(window, text='Symbols', onvalue=1, offvalue=0)
 Descript = tk.Label(window, text="Length of the Password")
-Password_length = tk.Scale(window, from_=5, to=20, orient='horizontal')
-PassDescript = tk.Button(window, text="Generate Password", command=lambda: user_choice("op1))
+Pass_length = tk.Scale(window, from_=5, to=20, orient='horizontal')
+PassDescript = tk.Button(window, text="Generate Password", command=generator)
+GenPass = tk.Entry(window, width=40)
 
-GenPass = tk.Label(window, width=40, height='3')
-
-loadassets()
+build()
 
