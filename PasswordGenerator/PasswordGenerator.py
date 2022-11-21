803e88ec54b16a59186a2e0108dc16dc2e6c9570
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-11-21 16:33:35 +0000

    work on project

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -1,79 +1,67 @@
 import tkinter as tk
 import random
 import string
+import pyperclip
+
+
+def generator():
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    numbers = string.digits
+    symbols = string.punctuation
+
+    GenPass.delete(0, 'end')
+
+    all_characters = lowercase+uppercase+numbers+symbols
+    password_length = Pass_length.get()
+
+    if choice.get() == 1:
+        GenPass.insert(0, random.sample(lowercase + uppercase, password_length))
+
+    elif choice.get() == 2:
+        GenPass.insert(0, random.sample(lowercase + uppercase + numbers, password_length))
+
+    elif choice.get() == 3:
+        GenPass.insert(0, random.sample(all_characters, password_length))
+
+
+def copy():
+    random_password = GenPass.get()
+    random_password = random_password.replace(" ", "")
+    pyperclip.copy(random_password)
+
+
+def build():
+    description.pack()
+    weak_radioButton.pack()
+    medium_radioButton.pack()
+    strong_radioButton.pack()
+    Descript.pack()
+    Pass_length.pack()
+    PassDescript.pack()
+    GenPass.pack()
+    Copy_button.pack()
+    window.mainloop()
+
 
 window = tk.Tk()
 window.title('Password Generator')
 window.geometry('500x500')
 
-
-def generator():
-    lower = string.ascii_lowercase
-    upper = string.ascii_uppercase
-    numbers = string.digits
-    symbols = string.punctuation
-    all_characters = ''
-
-    option1 = op1.getvar()
-    option2 = op2.getvar()
-    option3 = op3.getvar()
-    option4 = op4.getvar()
-    password_length = Pass_length.get()
-    count = 0
-
-    print(option1)
-    print(op1)
-    print(lower)
-    if option1.get():
-        print(lower)
-        all_characters += lower
-        count += 1
-    if option2 == 1:
-        all_characters += upper
-        count += 1
-    if option3 == 1:
-        all_characters += numbers
-        count += 1
-    if option4 == 1:
-        all_characters +=
-        count += 1
-
-    for i in range(password_length):
-        for x in range(x)
-            password_length -= 1
-            if
-        for y in range(y)
-
-
-    GenPass.insert(0, all_characters)
-    print(all_characters)
-
-
-def build():
-    description.pack()
-    op1.pack()
-    op2.pack()
-    op3.pack()
-    op4.pack()
-    Descript.pack()
-    Pass_length.pack()
-    PassDescript.pack()
-    GenPass.pack()
-    window.mainloop()
-
+choice = tk.IntVar()
 
 description = tk.Label(window, bg='white', width=50, height='5', text="This application is for generating passwords.\n"
                                                             "Please select from the checkboxes below your preferences")
 
 
-op1 = tk.Checkbutton(window, text='Lower-Case Characters', onvalue=1, offvalue=0)
-op2 = tk.Checkbutton(window, text='Upper-Case Characters', onvalue=1, offvalue=0)
-op3 = tk.Checkbutton(window, text='Numbers', onvalue=1, offvalue=0)
-op4 = tk.Checkbutton(window, text='Symbols', onvalue=1, offvalue=0)
+weak_radioButton = tk.Radiobutton(window, text='Weak', value=1, variable=choice)
+choice.set(1)
+medium_radioButton = tk.Radiobutton(window, text='Medium', value=2, variable=choice)
+strong_radioButton = tk.Radiobutton(window, text='Strong', value=3, variable=choice)
 Descript = tk.Label(window, text="Length of the Password")
-Pass_length = tk.Scale(window, from_=5, to=20, orient='horizontal')
+Pass_length = tk.Scale(window, from_=7, to=20, orient='horizontal')
 PassDescript = tk.Button(window, text="Generate Password", command=generator)
 GenPass = tk.Entry(window, width=40)
+Copy_button = tk.Button(window, text="Copy Password", command=copy)
 
 build()
-
